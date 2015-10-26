5c9761f9f8de55083b70b0cb514ca18075e6751b
Deric Mendez <deric@cremalab.com>
2015-10-26 14:41:40 -0500

    remove old directive refernece

--- client/js/directives/index.js
+++ client/js/directives/index.js
@@ -1,7 +1,6 @@
 var app = require('angular').module('beRoyaler');
 
 app.directive('file', require('./file'));
-app.directive('fileupload', ['upload', '$window', 'image', require('./fileupload')]);
 app.directive('draggable', require('./draggable'));
 app.directive('imagecrop', ['image', '$location', require('./imagecrop')]);
 app.directive('imageadd', ['image', '$location', require('./imageadd')]);
